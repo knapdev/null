1d41c50a3640228baebb01c3d30ce27b73fe8eef
knapdev <knapdev@gmail.com>
2022-06-06 18:42:28 -0500

    Smooth out camera look

--- project/player/states/player_idle_state.gd
+++ project/player/states/player_idle_state.gd
@@ -23,5 +23,7 @@ func physics_process(delta: float):
 	var look_input_dir = context.get_look_input()
 	
 	var camrig_pivot = context.get_node("CamRig/Pivot")
-	camrig_pivot.get_child(0).rotation.y = camrig_pivot.rotation.y - deg2rad(60 * look_input_dir.x)
-	camrig_pivot.get_child(0).rotation.x = camrig_pivot.rotation.x - deg2rad(30 * look_input_dir.y)
+	var cam_rot_y = camrig_pivot.get_child(0).rotation.y
+	var cam_rot_x = camrig_pivot.get_child(0).rotation.x
+	camrig_pivot.get_child(0).rotation.y = lerp_angle(cam_rot_y, camrig_pivot.rotation.y - deg2rad(60 * look_input_dir.x), 0.15)
+	camrig_pivot.get_child(0).rotation.x = lerp_angle(cam_rot_x, camrig_pivot.rotation.x - deg2rad(30 * look_input_dir.y), 0.15)
